9d8aabecf892434fc2cf6f9cdce77937cd4535c0
giangbimin <gianghoang.dev@gmail.com>
2017-05-30 00:08:13 +0700

    markdown with rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all.page(params[:page]).per(5)
+    @articles = Article.all.order('created_at DESC').page(params[:page]).per(5)
   end
 
   # GET /articles/1
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  class ColorCode < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+
+  def markdown(content)
+    opitions = {
+      hard_wrap: true,
+      filter_html: true
+    }
+
+    extensions = {
+      space_after_headers: true,
+      fenced_code_blocks: true,
+      autolink: true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      no_intra_emphasis: true,
+      disable_indented_code_blocks: true,
+      lax_spacing: true,
+      strikethrough: true,
+      superscript: true
+    }
+    renderer = ColorCode.new(opitions)
+    Redcarpet::Markdown.new(renderer, extensions).render(content).html_safe
+  end
 end
