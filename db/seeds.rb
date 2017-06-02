c4fd7efb59db41c4e38efdcc8a3f6a9c0570b541
giangbimin <gianghoang.dev@gmail.com>
2017-06-03 02:15:16 +0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
-100.times do
-  Article.create({ title: Faker::Name.title, body: Faker::Lorem.paragraph })
+10.times do
+  User.create({ email: Faker::Internet.email, password: '12345678', password_confirmation: '12345678', name: Faker::Name.name})
+end
+User.all.each do |user|
+  10.times do
+    user.articles.create(
+      title: Faker::Book.title,
+      description: Faker::Lorem.paragraph(2, false, 4),
+      body: Faker::Lorem.paragraph(7, false, 4),
+      tag_list: (Faker::Job.key_skill+', '+Faker::Job.key_skill+', '+Faker::Job.key_skill).to_s)
+  end
+end
+Article.all.each do |article|
+  article.comments.create(user_id: User.first.id, body: Faker::Lorem.paragraph(2))
+  article.comments.create(user_id: User.second.id, body: Faker::Lorem.paragraph(2))
 end
