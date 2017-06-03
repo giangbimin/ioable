f1f792784a081ecad9f7faab97bb2197fceff17b
giangbimin <gianghoang.dev@gmail.com>
2017-06-03 11:50:55 +0700

    fix author controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,11 +1,11 @@
-class AtthorsController < ApplicationController
+class AuthorsController < ApplicationController
   def show
     @author = User.find(params[:id])
-    @articles = Articles.where(user_id: @author.id).order('created_at DESC')
+    @articles = Article.where(user_id: @author.id).order('created_at DESC')
                        .page(params[:page]).per(5)
   end
 
   def index
-  	@authors = User.all
+    @authors = User.all
   end
 end
