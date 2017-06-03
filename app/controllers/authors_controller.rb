7f2b89f518568baefee8a36ad1e187b361af818f
giangbimin <gianghoang.dev@gmail.com>
2017-06-03 12:24:31 +0700

    fix count

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,6 +3,7 @@ class AuthorsController < ApplicationController
     @author = User.find(params[:id])
     @articles = Article.where(user_id: @author.id).order('created_at DESC')
                        .page(params[:page]).per(5)
+    @count = Article.where(user_id: @author.id).count
   end
 
   def index
